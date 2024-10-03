362c6b3edff6ec3f7a24d246449b259eb2a8b16f
Fabian Lapôtre <lapotrefabian@gmail.com>
2024-10-03 12:48:13 +0200

    fork: clean project to make a base to create co2 measurement device

--- app/src/main.c
+++ app/src/main.c
@@ -4,77 +4,61 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
-
-#include <app/drivers/blink.h>
+#include <zephyr/drivers/gpio.h>
 
 #include <app_version.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
+    #define LED0_PIN    DT_GPIO_PIN(LED0_NODE, gpios)
+    #define LED0_FLAGS  DT_GPIO_FLAGS(LED0_NODE, gpios)
+#else
+    #error "Unsupported board: led0 devicetree alias is not defined"
+#endif
 
 int main(void)
 {
-	int ret;
-	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = { 0 }, val;
+		printk("Zephyr CO2 measurement device base Application %s\n", APP_VERSION_STRING);
+        const struct device *const dev = DEVICE_DT_GET(DT_GPIO_CTLR(LED0_NODE, gpios));
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+        int ret;
+        bool led_state = true;
 
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
+        if (!device_is_ready(dev)) {
+				LOG_ERR("Sensor not ready");
+                return 0;
+        }
 
-	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-	if (!device_is_ready(blink)) {
-		LOG_ERR("Blink LED not ready");
-		return 0;
-	}
+        ret = gpio_pin_configure(dev, LED0_PIN, GPIO_OUTPUT);
+        if (ret < 0) {
+				LOG_ERR("Could not configure gpio (%d)", ret);
+                return 0;
+        }
 
-	ret = blink_off(blink);
-	if (ret < 0) {
-		LOG_ERR("Could not turn off LED (%d)", ret);
-		return 0;
-	}
+        ret = gpio_pin_toggle(dev, LED0_PIN);
+        if (ret < 0) {
+				LOG_ERR("Could not toggle led (%d)", ret);
+                return 0;
+        }
 
-	printk("Use the sensor to change LED blinking period\n");
+        while (1) {
+                ret = gpio_pin_toggle(dev, LED0_PIN);
+                if (ret < 0) {
+						LOG_ERR("Could not toggle led (%d)", ret);
+                        return 0;
+                }
 
-	while (1) {
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		if ((last_val.val1 == 0) && (val.val1 == 1)) {
-			if (period_ms == 0U) {
-				period_ms = BLINK_PERIOD_MS_MAX;
-			} else {
-				period_ms -= BLINK_PERIOD_MS_STEP;
-			}
-
-			printk("Proximity detected, setting LED period to %u ms\n",
-			       period_ms);
-			blink_set_period_ms(blink, period_ms);
-		}
-
-		last_val = val;
-
-		k_sleep(K_MSEC(100));
-	}
-
-	return 0;
+                led_state = !led_state;
+                printf("LED state: %s\n", led_state ? "ON" : "OFF");
+                k_msleep(SLEEP_TIME_MS);
+        }
+        return 0;
 }
-
